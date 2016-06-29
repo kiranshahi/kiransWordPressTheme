5361972a3b217dbd34bec617d6ab8722b08d3e9e
Kiran Shahi <mail@kirans.com.np>
2016-06-29 23:53:50 +0545

    special-template.php page has be styled a bit.

--- special-template.php
+++ special-template.php
@@ -1,5 +1,9 @@
 <?php  
 
+/**
+Template Name: Special Layout
+**/
+
 	get_header();
 
 
@@ -9,6 +13,22 @@
 
 		<article class="post page">
 			<h2><?php the_title(); ?></h2>
+
+
+			<div class="info-box">
+			
+				<h4>Disclaimer Title</h4>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
+
+			</div>
+
+
 			<?php the_content(); ?>
 		</article>
 
