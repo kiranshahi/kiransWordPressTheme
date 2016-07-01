fa51a69741d067c8554afe761e2273876edf1da6
Kiran Shahi <mail@kirans.com.np>
2016-07-01 15:31:35 +0545

    Child page function has been added.

--- functions.php
+++ functions.php
@@ -10,5 +10,24 @@ add_action('wp_enqueue_scripts', 'kiransThemeResources');
 
 register_nav_menus(array(
 	'primary' => __( 'Primary Menu' ),
-	'footer'  => __( 'Footer Menu'),
+	'footer'  => __( 'Footer Menu' ),
 	));
+
+//Get top ancestor
+function get_top_ancestor_id() {
+
+
+	global $post;
+
+
+	if ($post->post_parent) {
+
+
+		$ancestors = array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];
+
+	}
+
+	return $post->ID;
+
+}
--- page.php
+++ page.php
@@ -8,6 +8,31 @@
 		while (have_posts()) : the_post(); ?>
 
 		<article class="post page">
+
+			<nav class="site-nav children-links clearfix">
+
+				<span class="parent-link"><a href="<?php get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+
+
+				<ul>
+
+					<?php  
+
+						$args = array(
+
+							'child_of' => get_top_ancestor_id(),
+							'title_li' => '',
+
+							);
+
+					?>
+
+					<?php wp_list_pages($args); ?>
+
+				</ul>
+
+			</nav>
+
 			<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
 		</article>
